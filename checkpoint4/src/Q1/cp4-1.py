db2ad305e37bdc9a9274f1a8e14833fec82de963
chend666 <44977686+chend666@users.noreply.github.com>
2021-11-20 16:24:09 -0600

    Update cp4-1.py

--- checkpoint4/src/Q1/cp4-1.py
+++ checkpoint4/src/Q1/cp4-1.py
@@ -3,21 +3,7 @@ import matplotlib.pyplot as plt
 import psycopg2
 import numpy as np
 import random
-# G = nx.petersen_graph()
-
-# subax1 = plt.subplot(121)
-
-# nx.draw(G, with_labels=True, font_weight='bold')
-
-# subax2 = plt.subplot(122)
-
-# nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
-
-# plt.show()  
-
-#subax1 = plt.subplot(121)
-
-#plt.show()  
+import seaborn as sns
 
 conn = psycopg2.connect(host="codd01.research.northwestern.edu", port = 5432, database="postgres", user="cpdbstudent", password="DataSci4AI")
 cur = conn.cursor()
@@ -33,45 +19,10 @@ unit_id = []
 for i in q2: 
   unit_id.append(i[0])
 
-color = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-             for i in range(len(unit_id))]
+color = sns.color_palette("Spectral", 18)
 
-# officer_1 = []
-# for i in officers:
-#   if i[1] == unit_id[0]:
-#     officer_1.append(i[0])
-
-# officer_2 = []
-# for i in officers:
-#   if i[1] == unit_id[1]:
-#     officer_2.append(i[0])
-
-# G = nx.Graph()
-# fig = plt.figure(figsize=(80, 40))
-
-# for i in range(0,10):
-#   isCenter = True
-#   center = 0
-#   for j in range(0,len(officers)):
-#     if officers[j][1] == unit_id[i]:
-#       if isCenter:
-#         center = officers[j][0]
-#         isCenter = False
-#       else:
-#         G.add_edge(center,officers[j][0])
-
-# pos = nx.spring_layout(G,iterations = 20)
-# for i in range(0,10):
-#   temp = []
-#   for j in range(0,len(officers)):
-#     if officers[j][1] == unit_id[i]:
-#       temp.append(officers[j][0])
-#   nx.draw_networkx(G.subgraph(temp),pos = pos,with_labels = False,node_color = color[i],node_size = 60)
-
-# plt.show()
-
-cur.execute("""select id,ceil(current_salary/10000)*10000 as salary,complaint_percentile from data_officer
-where current_salary is not NULL and complaint_percentile is not NULL""")
+cur.execute("""select id,ceil(current_salary/10000)*10000 as salary,complaint_percentile,last_unit_id from data_officer
+where current_salary is not NULL and complaint_percentile is not NULL and last_unit_id is not NULL""")
 
 result = cur.fetchall()
 
@@ -81,4 +32,125 @@ for i in result:
   if i[1] not in salary:
     salary.append(i[1])
 
+salary.sort(reverse=True)
 print(salary)
+
+G = nx.Graph()
+fig = plt.figure(figsize=(80, 60), dpi=80)
+
+isCenter = True
+center = 0
+temp = []
+color_map = []
+for i in result:
+  if i[2] == 0:
+    if isCenter:
+      center = i[0]
+      isCenter = False
+      temp.append(center)
+    else:
+      G.add_edge(center,i[0])
+      temp.append(i[0])
+    color_map.append(color[salary.index(i[1])])
+
+isCenter = True
+center = 0
+temp1 = []
+color_map1 = []
+for i in result:
+  if i[2] >0 and i[2] < 20:
+    if isCenter:
+      center = i[0]
+      isCenter = False
+      temp1.append(center)
+    else:
+      G.add_edge(center,i[0])
+      temp1.append(i[0])
+    color_map1.append(color[salary.index(i[1])])
+
+isCenter = True
+center = 0
+temp2 = []
+color_map2 = []
+for i in result:
+  if i[2] >20 and i[2] < 40:
+    if isCenter:
+      center = i[0]
+      isCenter = False
+      temp2.append(center)
+    else:
+      G.add_edge(center,i[0])
+      temp2.append(i[0])
+    color_map2.append(color[salary.index(i[1])])
+
+isCenter = True
+center = 0
+temp3 = []
+color_map3 = []
+for i in result:
+  if i[2] >40 and i[2] < 60:
+    if isCenter:
+      center = i[0]
+      isCenter = False
+      temp3.append(center)
+    else:
+      G.add_edge(center,i[0])
+      temp3.append(i[0])
+    color_map3.append(color[salary.index(i[1])])
+
+isCenter = True
+center = 0
+temp3 = []
+color_map3 = []
+for i in result:
+  if i[2] > 40 and i[2] < 60:
+    if isCenter:
+      center = i[0]
+      isCenter = False
+      temp3.append(center)
+    else:
+      G.add_edge(center,i[0])
+      temp3.append(i[0])
+    color_map3.append(color[salary.index(i[1])])
+
+isCenter = True
+center = 0
+temp4 = []
+color_map4 = []
+for i in result:
+  if i[2] >60 and i[2] < 80:
+    if isCenter:
+      center = i[0]
+      isCenter = False
+      temp4.append(center)
+    else:
+      G.add_edge(center,i[0])
+      temp4.append(i[0])
+    color_map4.append(color[salary.index(i[1])])
+
+isCenter = True
+center = 0
+temp5 = []
+color_map5 = []
+for i in result:
+  if i[2] >80 and i[2] < 100:
+    if isCenter:
+      center = i[0]
+      isCenter = False
+      temp5.append(center)
+    else:
+      G.add_edge(center,i[0])
+      temp5.append(i[0])
+    color_map5.append(color[salary.index(i[1])])
+
+pos = nx.spring_layout(G,iterations = 10)
+nx.draw_networkx(G.subgraph(temp),pos = pos,node_color = color_map,with_labels = False,node_size = 30)
+nx.draw_networkx(G.subgraph(temp1),pos = pos,node_color = color_map1,with_labels = False,node_size = 30)
+nx.draw_networkx(G.subgraph(temp2),pos = pos,node_color = color_map2,with_labels = False,node_size = 30)
+nx.draw_networkx(G.subgraph(temp3),pos = pos,node_color = color_map3,with_labels = False,node_size = 30)
+nx.draw_networkx(G.subgraph(temp4),pos = pos,node_color = color_map4,with_labels = False,node_size = 30)
+nx.draw_networkx(G.subgraph(temp5),pos = pos,node_color = color_map5,with_labels = False,node_size = 30)
+plt.show()
+    
+
+print('OK')
